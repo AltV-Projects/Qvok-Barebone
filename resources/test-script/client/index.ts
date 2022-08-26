3ad7986af80d5346254d6bb2fa98abac8936f711
Franze, Mario <mario.franze@gmail.com>
2022-08-26 21:22:36 +0200

    Removed default scripts

--- resources/test-script/client/index.ts
+++
@@ -1,7 +0,0 @@
-import { emitServer, on, Player } from "alt-client";
-
-const player: Player = Player.local;
-
-on("connectionComplete", () => {
-  emitServer("client::event::connectionComplete");
-});
